b6f6e51e39f38ac20e21d041fd7e193f5f2f33a5
Stefano Mondino <stefano.mondino.dev@gmail.com>
2017-01-05 12:20:39 +0100

    Initial Commit

---
+++ Pax/Pax.h
@@ -0,0 +1,19 @@
+//
+//  Pax.h
+//  Pax
+//
+//  Created by Stefano Mondino on 05/01/17.
+//  Copyright © 2017 Synesthesia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pax.
+FOUNDATION_EXPORT double PaxVersionNumber;
+
+//! Project version string for Pax.
+FOUNDATION_EXPORT const unsigned char PaxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pax/PublicHeader.h>
+
+
